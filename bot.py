50a5e98a71c5089dfdf04dc4d6e328c04e84a0ea
AydarGataullin7 <aydarcr7@gmail.com>
2026-08-13 09:29:53 +0500

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -2,10 +2,12 @@ import logging
 import os
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
+from google.cloud import dialogflow_v2 as dialogflow
 from dotenv import load_dotenv
 load_dotenv()
 
 updater = Updater(token=os.getenv("TG_BOT_TOKEN"))
+PROJECT_ID = os.getenv("PROJECT_ID")
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -13,7 +15,19 @@ def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Здравствуйте!")
 
 def echo(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    user_text = update.message.text
+    session_id = str(update.effective_chat.id)
+    reply = detect_intent(PROJECT_ID, session_id, user_text)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply)
+
+def detect_intent(project_id, session_id, text):
+    client = dialogflow.SessionsClient()
+    session = client.session_path(project_id, session_id)
+    text = dialogflow.TextInput(text=text, language_code="ru")
+    query_input = dialogflow.QueryInput(text=text)
+    response = client.detect_intent(request={'session': session, 'query_input': query_input})
+    response_text = response.query_result.fulfillment_text
+    return response_text
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 start_handler = CommandHandler('start', start)
