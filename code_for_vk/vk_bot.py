14e134dbf09038e76c67915164ce67544fb82717
AydarGataullin7 <aydarcr7@gmail.com>
2026-08-16 18:12:06 +0500

    add DialogFlow to VK bot

--- code_for_vk/vk_bot.py
+++ code_for_vk/vk_bot.py
@@ -1,22 +1,39 @@
 import random
 import vk_api
 import os
+from google.cloud import dialogflow_v2 as dialogflow
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 load_dotenv()
+project_id = os.getenv('PROJECT_ID')
 
-def echo(event, vk_api):
+
+def echo(event, vk_api, project_id):
+    session_id = str(event.user_id)
+    reply = detect_intent(project_id, session_id, event.text)
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=reply,
         random_id=random.randint(1, 1000)
     )
 
-if __name__=='__main__':
+
+def detect_intent(project_id, session_id, text):
+    client = dialogflow.SessionsClient()
+    session = client.session_path(project_id, session_id)
+    text = dialogflow.TextInput(text=text, language_code="ru")
+    query_input = dialogflow.QueryInput(text=text)
+    response = client.detect_intent(
+        request={'session': session, 'query_input': query_input})
+    response_text = response.query_result.fulfillment_text
+    return response_text
+
+
+if __name__ == '__main__':
     vk_session = vk_api.VkApi(token=os.getenv("VK_TOKEN"))
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            echo(event, vk_api, project_id)
